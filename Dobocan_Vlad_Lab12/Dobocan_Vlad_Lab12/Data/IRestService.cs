8e35984e7a84104ec8b64fb042396432ab75620e
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2021-01-05 20:12:41 +0200

    Add project files.

---
+++ Dobocan_Vlad_Lab12/Dobocan_Vlad_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using Dobocan_Vlad_Lab12.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dobocan_Vlad_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
